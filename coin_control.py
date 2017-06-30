89d8020ef8b7c82aa0a0b3c26bf07e6a04a7a870
Oskar <oskar@szakinnis.de>
2017-06-30 22:05:10 +0200

    testing more shit

--- coin_control.py
+++ coin_control.py
@@ -1,23 +1,37 @@
 import RPi.GPIO as GPIO
 import time
+from threading import Timer
 
 switch_pin = 7 # the interrupt gpio pin
 screen_timeout = 10 # screen timeout in minutes
+total_passes = 0 # number of passes for the timer
 
+def insert_coin(pin):
+    global total_passes
+    if (total_passes <= 0):
+        print("turn on screen now")
+        timer.start()
+    else:
+        pass
+    total_passes += 1
 
+def timer_finish():
+    total_passes -= 1
+    if total_passes <= 0:
+        print("turn screen off again")
+    else:
+        pass
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(switch_pin, GPIO.IN)
-
-def interrupt_callback(pin):
-	print "turn off screen now"
-	time.sleep(5)
-	print "turn screen on again"
 
 def loop():
 	while (1):
 		time.sleep(1)
 
-GPIO.add_event_detect(switch_pin, GPIO.RISING, callback=interrupt_callback)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(switch_pin, GPIO.IN)
+
+GPIO.add_event_detect(switch_pin, GPIO.RISING, callback=insert_coin)
+timer = Timer(screen_timeout*60.0, timer_finish)
 
 loop()
