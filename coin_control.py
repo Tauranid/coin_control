bb2dc779fe7f32b7ad8a44df433d406c2cc9c53b
Ferdinand <frrdi.do@gamil.com>
2017-06-30 20:35:40 +0000

    update - is theoretically working now :^)

--- coin_control.py
+++ coin_control.py
@@ -2,25 +2,48 @@ import RPi.GPIO as GPIO
 import time
 from threading import Timer
 
+#CONSTANTS
 switch_pin = 7 # the interrupt gpio pin
 screen_timeout = 10 # screen timeout in minutes
+
+#TECHNICAL CONSTANTS (Don't change once working as intended)
+switch_debounce = 0.5 # switch debounce time in seconds
+debounce_flag = 0
+
 total_passes = 0 # number of passes for the timer
+
+
+def timer_debounce_finish():
+    global debounce_flag
+    debounce_flag = 0
 
 def insert_coin(pin):
     global total_passes
+    global debounce_flag
+
     if (total_passes <= 0):
         print("turn on screen now")
-        timer.start()
+        screen_timer = Timer(3, timer_screen_finish)
+        screen_timer.start()
     else:
         pass
-    total_passes += 1
 
-def timer_finish():
+    if (debounce_flag == 0):
+        debounce_timer = Timer(switch_debounce, timer_debounce_finish)
+	debounce_flag = 1
+        total_passes += 1
+        debounce_timer.start()
+        print("increase passes to " + str(total_passes))
+
+def timer_screen_finish():
+    global total_passes
     total_passes -= 1
+    print("decrease passes to " + str(total_passes))
     if total_passes <= 0:
         print("turn screen off again")
     else:
-        pass
+        screen_timer = Timer(3, timer_screen_finish)
+	screen_timer.start()
 
 
 def loop():
@@ -32,6 +55,5 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(switch_pin, GPIO.IN)
 
 GPIO.add_event_detect(switch_pin, GPIO.RISING, callback=insert_coin)
-timer = Timer(screen_timeout*60.0, timer_finish)
 
 loop()
