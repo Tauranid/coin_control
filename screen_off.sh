0db7c355d114e28a444c349aa81e2420a19696d4
Ferdinand <frrdi.do@gamil.com>
2017-07-09 17:15:20 +0000

    almost done

--- screen_off.sh
+++ screen_off.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 emuchild=$(pgrep -f /opt/retropie/configs/all/autostart.sh)
-emuparent=$(ps -o ppid= -p $emuchild)
-kill -STOP -- -$emuparent
+emuparent=$((0-$(ps -o ppid= -p $emuchild)))
+kill -STOP -- $emuparent
 #vcgencmd display_power 0
